24a8238ee61ce7f84cf8e0fb71b8a1f90ecef1f3
Ian kangethe <iankangethe@Ians-MacBook-Pro.local>
2023-08-07 05:46:27 +0300

    clean slate

---
+++ src/main/kotlin/com/thinkauth/thinkfusionauth/ThinkFusionAUthApplication.kt
@@ -0,0 +1,11 @@
+package com.thinkauth.thinkfusionauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThinkFusionAUthApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThinkFusionAUthApplication>(*args)
+}
---
+++ src/test/kotlin/com/thinkauth/thinkfusionauth/ThinkFusionAUthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.thinkauth.thinkfusionauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThinkFusionAUthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
