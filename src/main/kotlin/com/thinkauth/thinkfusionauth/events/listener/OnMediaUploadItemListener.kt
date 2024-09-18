cca31d8e0da01bf4da8fbaa65df41bc2ac386374
kangethe ndungu <kangethendungu@kangethes-Laptop.local>
2024-09-18 17:45:14 +0300

    media upload async

--- src/main/kotlin/com/thinkauth/thinkfusionauth/events/listener/OnMediaUploadItemListener.kt
+++ src/main/kotlin/com/thinkauth/thinkfusionauth/events/listener/OnMediaUploadItemListener.kt
@@ -14,11 +14,7 @@ import kotlin.math.log
 
 @Component
 class OnMediaUploadItemListener(
-    private val fileManagerService: StorageService,
-    private val userManagementService: UserManagementService,
     private val mediaEntityService: MediaEntityService,
-    private val audioManagementService: AudioCollectionService,
-    @Value("\${minio.bucket}") private val bucketName: String
 ) : ApplicationListener<OnMediaUploadItemEvent> {
 
     private val logger: Logger = LoggerFactory.getLogger(OnMediaUploadItemListener::class.java)
@@ -28,65 +24,8 @@ class OnMediaUploadItemListener(
 
         logger.info("uploading media")
 
-        uploadMedia(event)
+        mediaEntityService.uploadMedia(event)
         logger.info("Finished uploading media")
     }
-    @Async
-    private fun uploadMedia(event: OnMediaUploadItemEvent) {
-        try {
-            val multipartFile = event.file
-            val path = event.copyLocation
-            val resource = event.resource
-            val sentenceId = event.sentenceId
-            val businessId = event.businessId
-            val genderState = event.genderState
 
-//        if (authentication !is AnonymousAuthenticationToken) {
-//            val userPrincipal = authentication.principal as String
-//            println("User principal name =" + userPrincipal.username)
-//            println("Is user enabled =" + userPrincipal.isEnabled)
-//        }
-//        var bucko:String = when(resource){
-//            BucketName.BUSINESS_PROFILE_PIC ->{
-//                "$bucketName/"+ BucketName.BUSINESS_PROFILE_PIC+"/"+path.name
-//            }
-//
-//            BucketName.STT_UPLOAD ->{
-//                "$bucketName/"+ BucketName.STT_UPLOAD+"/"+path.name
-//            }
-//
-//            BucketName.VOICE_COLLECTION ->{
-//                "$bucketName/"+ BucketName.VOICE_COLLECTION+"/"+path.name
-//            }
-//
-//            BucketName.USER_ACCOUNT_PROFILE->{
-//                "$bucketName/"+ BucketName.USER_ACCOUNT_PROFILE+"/"+path.name
-//            }
-//        }
-//        var bucko = mediaFullPath(resource, multipartFile.name).absolutePathString()
-            logger.info("path: $path")
-            val response = fileManagerService.uploadFile(bucketName, path, multipartFile.inputStream)
-            logger.info("uploading response: ${response.toString()}")
-            val user = userManagementService.fetchLoggedInUserEntity()
-            logger.info("logged in user: ${user}")
-            val sentence = audioManagementService.getAudioCollectionById(sentenceId!!)
-            logger.info("sentence: $sentence")
-            val mediaEntity = MediaEntity(
-                mediaName = resource.name,
-                owner = user,
-                username = user.username ?: user.email,
-                mediaObject = response?.`object`()!!,
-                mediaPathId = path,
-                sentenceId = sentenceId,
-                actualSentence = sentence.sentence,
-                languageId = sentence.language.id,
-                languageName = sentence.language.languageName,
-                businessId = businessId,
-                genderState = genderState
-            )
-            mediaEntityService.saveMediaEntity(mediaEntity)
-        }catch (e:Exception){
-            logger.error("OnMediaUploadListener: ${e.toString()}")
-        }
-    }
 }
--- src/main/kotlin/com/thinkauth/thinkfusionauth/services/MediaEntityService.kt
+++ src/main/kotlin/com/thinkauth/thinkfusionauth/services/MediaEntityService.kt
@@ -2,12 +2,18 @@ package com.thinkauth.thinkfusionauth.services
 
 import com.thinkauth.thinkfusionauth.entities.enums.MediaAcceptanceState
 import com.thinkauth.thinkfusionauth.entities.MediaEntity
+import com.thinkauth.thinkfusionauth.events.OnMediaUploadItemEvent
+import com.thinkauth.thinkfusionauth.events.listener.OnMediaUploadItemListener
 import com.thinkauth.thinkfusionauth.models.responses.LanguageRecordingsResponse
 import com.thinkauth.thinkfusionauth.models.responses.PagedResponse
 import com.thinkauth.thinkfusionauth.models.responses.UserLanguageRecordingsResponse
 import com.thinkauth.thinkfusionauth.repository.MediaEntityRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
@@ -15,9 +21,12 @@ class MediaEntityService(
     private val mediaEntityRepository: MediaEntityRepository,
     private val userManagementService: UserManagementService,
     private val audioCollectionService: AudioCollectionService,
-    private val languageService: LanguageService
+    private val languageService: LanguageService,
+    private val fileManagerService: StorageService,
+    private val audioManagementService: AudioCollectionService,
+    @Value("\${minio.bucket}") private val bucketName: String
 ) {
-
+    private val logger: Logger = LoggerFactory.getLogger(MediaEntityService::class.java)
     fun saveMediaEntity(mediaEntity: MediaEntity): MediaEntity {
         return mediaEntityRepository.save(mediaEntity)
     }
@@ -155,5 +164,63 @@ class MediaEntityService(
             content.totalElements,
             content.totalPages
         )
+    }
+    @Async
+    fun uploadMedia(event: OnMediaUploadItemEvent) {
+        try {
+            val multipartFile = event.file
+            val path = event.copyLocation
+            val resource = event.resource
+            val sentenceId = event.sentenceId
+            val businessId = event.businessId
+            val genderState = event.genderState
+
+//        if (authentication !is AnonymousAuthenticationToken) {
+//            val userPrincipal = authentication.principal as String
+//            println("User principal name =" + userPrincipal.username)
+//            println("Is user enabled =" + userPrincipal.isEnabled)
+//        }
+//        var bucko:String = when(resource){
+//            BucketName.BUSINESS_PROFILE_PIC ->{
+//                "$bucketName/"+ BucketName.BUSINESS_PROFILE_PIC+"/"+path.name
+//            }
+//
+//            BucketName.STT_UPLOAD ->{
+//                "$bucketName/"+ BucketName.STT_UPLOAD+"/"+path.name
+//            }
+//
+//            BucketName.VOICE_COLLECTION ->{
+//                "$bucketName/"+ BucketName.VOICE_COLLECTION+"/"+path.name
+//            }
+//
+//            BucketName.USER_ACCOUNT_PROFILE->{
+//                "$bucketName/"+ BucketName.USER_ACCOUNT_PROFILE+"/"+path.name
+//            }
+//        }
+//        var bucko = mediaFullPath(resource, multipartFile.name).absolutePathString()
+            logger.info("path: $path")
+            val response = fileManagerService.uploadFile(bucketName, path, multipartFile.inputStream)
+            logger.info("uploading response: ${response.toString()}")
+            val user = userManagementService.fetchLoggedInUserEntity()
+            logger.info("logged in user: ${user}")
+            val sentence = audioManagementService.getAudioCollectionById(sentenceId!!)
+            logger.info("sentence: $sentence")
+            val mediaEntity = MediaEntity(
+                mediaName = resource.name,
+                owner = user,
+                username = user.username ?: user.email,
+                mediaObject = response?.`object`()!!,
+                mediaPathId = path,
+                sentenceId = sentenceId,
+                actualSentence = sentence.sentence,
+                languageId = sentence.language.id,
+                languageName = sentence.language.languageName,
+                businessId = businessId,
+                genderState = genderState
+            )
+           saveMediaEntity(mediaEntity)
+        }catch (e:Exception){
+            logger.error("OnMediaUploadListener: ${e.toString()}")
+        }
     }
 }
